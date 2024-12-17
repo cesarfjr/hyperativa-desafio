202f6378097f453b36b9fd1ca565f1901cf013dc
Cesar Faustino Jr. <cesar.af.jr@gmail.com>
2024-12-17 11:16:34 -0300

    Initial commit with bare bones Spring Boot application, using hsqldb in memory.

---
+++ src/main/java/br/com/cesarfjr/hyperativa/backend/DesafioHyperativaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesarfjr.hyperativa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioHyperativaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioHyperativaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cesarfjr/hyperativa/backend/DesafioHyperativaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesarfjr.hyperativa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioHyperativaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
